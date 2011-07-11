4b9d6f8b8ab522eed661bd32717771f5b0af4242
David Golden <dagolden@cpan.org>
2011-07-11 18:52:49 -0400

    initial import of Dist::Zilla::Plugin::InsertCopyright

---
+++ lib/Dist/Zilla/Plugin/InsertCopyright.pm
@@ -0,0 +1,40 @@
+use 5.008001;
+use strict;
+use warnings;
+use utf8;
+
+package Dist::Zilla::Plugin::InsertCopyright;
+# ABSTRACT: No abstract given for Dist::Zilla::Plugin::InsertCopyright
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+__END__
+
+=for Pod::Coverage method_names_here
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use Dist::Zilla::Plugin::InsertCopyright;
+
+= DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+= USAGE
+
+Good luck!
+
+= SEE ALSO
+
+Maybe other modules do related things.
+
+=end wikidoc
+
+=cut
+
